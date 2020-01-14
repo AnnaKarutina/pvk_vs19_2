304e35359a39cb3a32076daac5b7cc11bad63b24
Anna Karutina <anna.karutina@khk.ee>
2020-01-14 14:03:17 +0200

    Loodud kontrolleri faili valik ja kasutusele võtmine

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -3,9 +3,19 @@
 
 class Core
 {
+  protected $currentController = 'Pages';
   public function __construct()
   {
     $url = $this->getUrl();
+    $controllerName = ucwords($url[0]);
+    $controllerFileName = '../app/controllers/'.$controllerName.'.php';
+    if(file_exists($controllerFileName)){
+      $this->currentController = $controllerName;
+      unset($url[0]);
+    }
+    require_once $controllerFileName;
+    $this->currentController = new $this->currentController;
+    echo $controllerFileName;
     echo '<pre>';
     print_r($url);
     echo '</pre>';
