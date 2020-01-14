6180caba7286847ccd77883c029b5a5c772ccabf
Anna Karutina <anna.karutina@khk.ee>
2020-01-14 13:40:38 +0200

    Küsime lingi andmed ja teeme vajalikud teisendused

---
+++ app/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+// laadime vajalikud raamatukogud
+require_once 'libraries/Core.php';
---
+++ app/libraries/Core.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class Core
+{
+  public function __construct()
+  {
+    $url = $this->getUrl();
+    echo '<pre>';
+    print_r($url);
+    echo '</pre>';
+  }
+
+  public function getUrl(){
+    if(isset($_GET['url'])){
+      $url = $_GET['url'];
+      $url = rtrim($url, '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
+    }
+  }
+}
