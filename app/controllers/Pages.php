027bfcb206828e84ea7e07524c1cdda0ac471ab3
Anna Karutina <anna.karutina@khk.ee>
2020-01-17 10:46:52 +0200

    Kontrolleri ja meetodi jooksev tuvastamine url andmete põhjal

---
+++ app/controllers/Pages.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class Pages
+{
+  /**
+   * Pages constructor.
+   */
+  public function __construct()
+  {
+    echo 'Pages is loaded<br>';
+  }
+
+  public function edit(){
+    echo 'edit method is loaded<br>';
+  }
+
+  public function index() {
+    echo 'index method is loaded<br>';
+  }
+}
