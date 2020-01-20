60f1a47ef09a3e1a00e7851e5f52c15efa4bb726
Anna Karutina <anna.karutina@khk.ee>
2020-01-20 16:36:57 +0200

    Lisame konstandid kasutusele

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,4 +1,6 @@
 <?php
+// laadime vajalikud konstandid
+require_once 'config/constants.php';
 // laadime vajalikud raamatukogud
 spl_autoload_register(function ($className) {
   require_once 'libraries/'.$className.'.php';
---
+++ app/config/constants.php
@@ -0,0 +1,7 @@
+<?php
+// projekti nimi
+define('PROJECT_NAME', 'pvk');
+// juur url
+define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
+// rakenduse juurkataloog
+define('APPROOT', dirname(dirname(__FILE__)));
